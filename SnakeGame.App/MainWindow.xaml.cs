426bd3cc79a11843c3ef57c32b5a0bebed278799
cnrdvy@gmail.com <cnrdvy@gmail.com>
2024-05-02 00:51:19 +1000

    ADD: Adding app resources UPD: Updating game UI ADD: Adding frontend handler logic

--- SnakeGame.App/MainWindow.xaml.cs
+++ SnakeGame.App/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SnakeGame.App
 {
@@ -16,9 +10,98 @@ namespace SnakeGame.App
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int _rows = 15, _columns = 15;
+        private readonly Image[,] _gridImages;
+        private readonly Dictionary<GridValueEnumeration, ImageSource> _gridValueToImage = new()
+        {
+            { GridValueEnumeration.Empty, Images.Empty },
+            { GridValueEnumeration.Snake, Images.Body },
+            { GridValueEnumeration.Food, Images.Food }
+        };
+        private readonly GameState _gameState;
+
         public MainWindow()
         {
             InitializeComponent();
+            _gridImages = SetupGrid();
+            _gameState = new GameState(_rows, _columns);
+        }
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[_rows, _columns];
+            GameGrid.Rows = _rows;
+            GameGrid.Columns = _columns;
+
+            for (var row = 0; row < _rows; row++)
+            {
+                for (var column = 0; column < _columns; column++)
+                {
+                    var image = new Image { Source = Images.Empty };
+                    images[row, column] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            return images;
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Draw();
+            await GameLoop();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_gameState.IsGameOver)
+                return;
+
+            switch (e.Key)
+            {
+                case Key.Left:
+                    _gameState.ChangeSnakeDirection(Direction.Left);
+                    break;
+                case Key.Right:
+                    _gameState.ChangeSnakeDirection(Direction.Right);
+                    break;
+                case Key.Up:
+                    _gameState.ChangeSnakeDirection(Direction.Up);
+                    break;
+                case Key.Down:
+                    _gameState.ChangeSnakeDirection(Direction.Down);
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        private async Task GameLoop()
+        {
+            while (!_gameState.IsGameOver)
+            {
+                await Task.Delay(100);
+                _gameState.MoveSnake();
+                Draw();
+            }
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+            ScoreText.Text = $"SCORE {_gameState.Score}";
+        }
+
+        private void DrawGrid()
+        {
+            for (var row = 0; row < _rows; row++)
+            {
+                for (var column = 0; column < _columns; column++)
+                {
+                    var gridValue = _gameState.Grid[row, column];
+                    _gridImages[row, column].Source = _gridValueToImage[gridValue];
+                }
+            }
         }
     }
 }

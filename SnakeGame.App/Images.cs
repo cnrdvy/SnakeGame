f486d05a1bda2e127ea53a9c47a59227858e587b
cnrdvy@gmail.com <cnrdvy@gmail.com>
2024-05-02 00:49:52 +1000

    ADD: Adding Images

---
+++ SnakeGame.App/Images.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SnakeGame.App;
+
+public static class Images
+{
+    public readonly static ImageSource Empty = LoadImage("Empty.png");
+    public readonly static ImageSource Body = LoadImage("Body.png");
+    public readonly static ImageSource DeadBody = LoadImage("DeadBody.png");
+    public readonly static ImageSource DeadHead = LoadImage("DeadHead.png");
+    public readonly static ImageSource Food = LoadImage("Food.png");
+    public readonly static ImageSource Head = LoadImage("Head.png");
+
+    private static ImageSource LoadImage(string filename) 
+        => new BitmapImage(new Uri($"Assets/{filename}", UriKind.Relative));
+}
